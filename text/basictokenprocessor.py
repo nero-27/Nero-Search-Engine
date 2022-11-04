b580e385a3a29270370011949f73b6df325abc8f
richa-patel-27 <75730936+richa-patel-27@users.noreply.github.com>
2022-11-04 12:44:41 -0700

    additional files milestone2

---
+++ text/basictokenprocessor.py
@@ -0,0 +1,38 @@
+from .tokenprocessor import TokenProcessor
+import re
+from porter2stemmer import Porter2Stemmer
+
+class BasicTokenProcessor(TokenProcessor):
+    """A BasicTokenProcessor creates terms from tokens by removing all non-alphanumeric characters 
+    from the token, and converting it to all lowercase."""
+    whitespace_re = re.compile(r"\W+")
+    
+    # def process_token(self, token : str) -> str:
+    #     return re.sub(self.whitespace_re, "", token).lower() # remove punctuations 
+        
+    def remove_hyphens(self, token):
+        list_of_tokens = []
+        if '-' in token:
+            list_of_tokens = token.split('-')
+            t = ''
+            for i in list_of_tokens:
+                t += i
+            list_of_tokens.append(t)
+        else:
+            list_of_tokens = [token]
+
+        return list_of_tokens
+
+    def process_token(self, list_of_tokens):
+        stemmer = Porter2Stemmer()
+        processed_list_of_tokens = []
+        for tok in list_of_tokens:
+            if tok != '':
+                tok = re.sub(self.whitespace_re, "", tok).lower()
+                tok = re.sub(r"^\W+|\W+$", "", tok)
+                tok = tok.replace("'","")
+                tok = tok.replace('"','')
+                processed_list_of_tokens.append(stemmer.stem(tok))        
+
+        return processed_list_of_tokens
+
