4b29487c66d730d075d2f5387b629a0e6d32b943
richa-patel-27 <75730936+richa-patel-27@users.noreply.github.com>
2022-11-04 11:09:03 -0700

    add milestone2 documents

---
+++ documents/textfiledocument.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import Iterable
+from .document import Document
+from io import StringIO
+
+class TextFileDocument(Document):
+    """
+    Represents a document that is saved as a simple text file in the local file system.
+    """
+    def __init__(self, id : int, path : Path):
+        super().__init__(id)
+        self.path = path
+
+    @property
+    def title(self) -> str:
+        return self.path.stem
+
+    def get_content(self) -> Iterable[str]:
+        return open(self.path)
+
+    @property
+    def get_string_content(self) -> Iterable[str]:
+        return open(self.path).read()
+
+    @property
+    def author(self) -> str:
+        return ''
+
+    @staticmethod
+    def load_from(abs_path : Path, doc_id : int) -> 'TextFileDocument' :
+        """A factory method to create a TextFileDocument around the given file path."""
+        return TextFileDocument(doc_id, abs_path)
